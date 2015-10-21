a4543172716ed1b5d6c4c90b37f7a180d0f50ab2
Vagrant User <reynaldo.manzano@spsmail.cuny.edu>
2015-10-21 06:23:07 +0000

    made adjustments based on Jenkins tests

--- task_01.py
+++ task_01.py
@@ -2,6 +2,4 @@
 # -*- coding: utf-8 -*-
 """Collect user input on the command line"""
 
-"""This function prompts users name then saves it within MY_ANSWER"""
-
 MY_ANSWER = raw_input("What is your name?  ")
--- task_05.py
+++ task_05.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 """A program that asks the user their blood pressure"""
 
-#"""This program allows a user to input thier blood pressure
-#and based on entry it will print out a message stating status"""
-
 BP = int(raw_input('What is your blood pressure?  '))
 """This entry below sets the stage for the first entry, starting at zero
 and accepts anything below or equal to 89."""
